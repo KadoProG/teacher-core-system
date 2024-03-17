f35ce3b72a2a67a1937cb3cd5aebe78eb6345cb9
KadoProG <splatrain@gmail.com>
2024-03-17 19:53:58 +0900

    huskyでカバーできなかったeslintの修正を反映

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import { ThemeRegistry } from '@/components/theme/themeRegistry';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ThemeRegistry } from '@/components/theme/themeRegistry';
 
 const inter = Inter({ subsets: ['latin'] });
 
