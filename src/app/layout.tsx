3a62434727b7d49314d11299377bf516a06c9cc3
KadoProG <splatrain@gmail.com>
2024-03-17 21:29:43 +0900

    update: 臨時的にダークモード対応themeでロード時画面がフラッシュする問題を緩和

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeRegistry } from '@/components/theme/themeRegistry';
+import '@/app/style.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
