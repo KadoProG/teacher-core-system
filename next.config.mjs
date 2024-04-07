ae951ec36d432260fa7159530eb0cb421898014f
KadoProG <splatrain@gmail.com>
2024-04-07 17:57:52 +0900

    Error: Page "/api/auth/[...nextauth]" is missing "generateStaticParams()" so it cannot be used with "output: export" config.というエラーが出たため修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Next.jsの設定をここに追加する
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: '/api/auth/[...nextauth]',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
