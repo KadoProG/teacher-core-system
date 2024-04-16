dd5d79bb44889284abe4aae4cc59936f42a1b4f6
KadoProG <splatrain@gmail.com>
2024-04-17 07:21:03 +0900

    一旦不要な部分を削除

--- src/components/commons/PrintLayoutPreview.tsx
+++ src/components/commons/PrintLayoutPreview.tsx
@@ -2,30 +2,8 @@
 import { Box, Button } from '@mui/material';
 import jsPDF from 'jspdf';
 import React from 'react';
-import { useReactToPrint } from 'react-to-print';
 import { PrintLayoutContainer } from '@/components/commons/PrintLayoutContainer';
 import './ipaexg-normal';
-
-/**
- * 印刷時のスタイル設定（印刷仕様に応じてスタイリングを調整します）
- * 　NOTE
- * 　・背景色/背景画像を表示させる設定（Chrome）：-webkit-print-color-adjust: exact
- * 　・pageのmargin指定でプレビュー時にデフォルト表示されるURLと時刻（ヘッダーとフッター）を表示エリアから外している
- *  https://zenn.dev/ikefukurou777/articles/606bf9f02cee7d より
- */
-const pageStyle = `
-    @page { 
-      size: auto;
-      margin: 5mm;
-    }
-    
-    @media print {
-      body { -webkit-print-color-adjust: exact; }
-      table { break-after: auto; }
-      tr    { break-inside:avoid; break-after:auto }
-      td    { break-inside:avoid; break-after:auto }
-    }
-  `;
 
 interface PrintLayoutPreviewProps {
   children: React.ReactNode;
@@ -40,13 +18,6 @@ export const PrintLayoutPreview: React.FC<PrintLayoutPreviewProps> = (
 ) => {
   const componentRef = React.useRef<HTMLDivElement>(null);
   const pdfRef = React.useRef(new jsPDF());
-  /**
-   * 印刷対象のコンポーネントを設定します
-   */
-  const reactToPrintContent = React.useCallback(() => {
-    if (!componentRef.current) return null;
-    return componentRef.current;
-  }, []);
 
   const handlePrint = () => {
     if (!componentRef.current) return;
@@ -63,11 +34,6 @@ export const PrintLayoutPreview: React.FC<PrintLayoutPreviewProps> = (
       windowWidth: 775,
     });
   };
-  // const handlePrint = useReactToPrint({
-  //   pageStyle, // 印刷のスタイリングを指定
-  //   content: reactToPrintContent, // 印刷エリアを指定
-  //   removeAfterPrint: true, // 印刷後に印刷用のiframeを削除する
-  // });
 
   return (
     <Box>
