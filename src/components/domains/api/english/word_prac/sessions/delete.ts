8c8e9539b0e82a2b0fbd6c2d1bcde806a48f35f5
KadoProG <splatrain@gmail.com>
2024-04-28 07:55:57 +0900

    #62 delete: nextauthとapiを削除

--- src/components/domains/api/english/word_prac/sessions/delete.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-export const sessionsDelete = async (
-  prismaIncludeTransaction?: PrismaIncludeTransaction
-) => {
-  const prisma = prismaIncludeTransaction ?? new PrismaClient();
-  await prisma.englishWordPracSession.deleteMany();
-};
--- src/components/domains/api/english/word_prac/words/delete.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-export const wordsDelete = async (
-  prismaIncludeTransaction?: PrismaIncludeTransaction
-) => {
-  const prisma = prismaIncludeTransaction ?? new PrismaClient();
-  await prisma.englishWordPracWord.deleteMany();
-};
